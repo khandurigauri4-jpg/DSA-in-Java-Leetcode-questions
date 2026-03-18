a11167b5adf8e274ba7ccf711cdb917bc5620c3e
khandurigauri4-jpg <khandurigauri4@gmail.com>
2026-03-18 21:14:10 +0530

    Intersection of two arrays using HashSet

---
+++ intersection.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer>set1 = new HashSet<>();
+        HashSet<Integer>resultSet = new HashSet<>();
+        for(int num : nums1)
+        {
+            set1.add(num);
+        }
+        for(int num : nums2)
+        {
+            if(set1.contains(num))
+            {
+                resultSet.add(num);
+            }
+        }
+        int arr[] = new int[resultSet.size()];
+        int i = 0;
+        for(int num : resultSet)
+        {
+            arr[i++] = num;
+        }
+        return arr;
+        
+    }
+
+public static void main(String args[])
+{
+    int nums1[] = {1 , 2 , 2, 1};
+    int nums2[] = {2 , 2};
+    System.out.println(intersection(nums1 , nums2));
+}
+}
